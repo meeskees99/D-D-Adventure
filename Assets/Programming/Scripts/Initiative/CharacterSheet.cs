34d08ed1c67b5adcccf8480bf4b33df5d593bf34
Xander Veninga <39034729+EliteForce1@users.noreply.github.com>
2023-09-13 21:29:05 +0200

    negeer de error

--- Assets/Programming/Scripts/Initiative/CharacterSheet.cs
+++ Assets/Programming/Scripts/Initiative/CharacterSheet.cs
@@ -2,8 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public class CharacterSheet : ScriptableObject
+public class CharacterSheet : MonoBehaviour 
 {
-    
+    [Header("Character Info")]
+    public string charaterName;
+    public GameObject characterModel;
+    public enum Sexes
+    {
+        Male,
+        Female
+    }
+    public Sexes sex;
+    public enum Class
+    {
+        Knight,
+        Ranger
+    }
+    public Class _class;
+    public int maxHitPoints,
+        hitPoints,
+        proficiency,
+        walking,
+        initiativeBonus,
+        armorClass;
+
+    [Header("Stats")]
+    public int strength;
+    public int dexterity,
+        constitution,
+        intelligence,
+        wisdom,
+        charisma;
 }
--- Assets/Programming/Scripts/Initiative/InitiativeHandler.cs
+++ Assets/Programming/Scripts/Initiative/InitiativeHandler.cs
@@ -1,8 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 
 public class InitiativeHandler : MonoBehaviour
 {
+    public List<CharacterSheet> characters;
+    public SortedList<GameObject, int> initiativeOrder;
+    public void Start()
+    {
+        SetInitiativeOrder();
+    }
 
+    private void SetInitiativeOrder()
+    {
+        if(initiativeOrder.Count != 0)
+        {
+            initiativeOrder.Clear();
+        }
+        
+        foreach(var character in characters)
+        {
+            int initiative = UnityEngine.Random.Range(0, 20) + character.initiativeBonus;
+            initiativeOrder.Add(character.characterModel, initiative);
+            Debug.Log(initiativeOrder);
+        }
+    }
 }
