67023813f9e35cc7cb471e6045c3644f98f03e6a
Xander Veninga <39034729+EliteForce1@users.noreply.github.com>
2023-09-22 15:10:58 +0200

    beginning save load system

---
+++ Assets/Programming/Scripts/SaveAndLoad/SavedData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SavedData
+{
+    //List of all players in campaign
+    [SerializeField] public List<(int, int, int, int, int, int)> playerStats = new List<(int, int, int, int, int, int)> ();
+    public List<Vector3> entityPos;
+}
