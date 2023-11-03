59590e9f5a78059e9eddf74c2b8530103c84761f
Gerrielofo <gerlof.reinink@gmail.com>
2023-11-03 15:37:10 +0100

    Update GameManager.cs

--- Assets/Programming/Scripts/GameManager.cs
+++ Assets/Programming/Scripts/GameManager.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
-public class GameManager : MonoBehaviour
+public class GameManager : NetworkBehaviour
 {
+    [SerializeField] GameObject dmCanvas;
+    [SerializeField] GameObject playerCanvas;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (NetworkManager.Singleton.LocalClient.ClientId == 0)
+        {
+            dmCanvas.SetActive(true);
+            playerCanvas.SetActive(false);
+        }
+        else
+        {
+            dmCanvas.SetActive(false);
+            playerCanvas.SetActive(true);
+        }
     }
 }
